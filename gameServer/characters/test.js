19c1b0c0ae18528414be6bf86764bec87b68c513
Alexey <yankovskyalexey123@yandex.ru>
2026-07-31 18:35:05 +0300

    added simplified knight character

--- gameServer/characters/test.js
+++ gameServer/characters/test.js
@@ -1,20 +1,36 @@
 export default {
-    name: "test",
-    health: 10,
+    name: "Knight",
+    health: 30,
     actions: {
-        act0: {
-            type: "noTarget",
-            mana: 10,
-            apply(state, unit) {
-                console.log("test");
-            }
-        },
-        act1: {
+        SwordAttack: {
             type: "enemyTarget",
             mana: 10,
             apply(state, unit, target) {
-                console.log("test");
+                if (state.units[unit].frontline && state.units[target].frontline) {
+                    state.units[target].damage(6);
+                }
+            }
+        },
+        SplashAttack: {
+            type: "noTarget",
+            mana: 15,
+            apply(state, unit) {
+                if (state.units[unit].frontline)
+                    for (target of state.units) if (state.isEnemy(state.player, target)) {
+                        if (state.units[target].frontline) {
+                          state.units[target].damage(4);
+                        }
+                    }
+                }
+            }
+        },
+        Shield: {
+            type: "noTarget",
+            mana: 12,
+            apply(state, unit) {
+                state.units[unit].heal(5)
             }
         }
+
     }
 };
